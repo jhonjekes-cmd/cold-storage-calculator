bf14d2117b810bd1a1eb09d8a00f785d9d340511
jhonjekes-cmd <jhonjekes@gmail.com>
2026-09-19 12:32:25 +0800

    feat: 新增微信小程序版本 - 项目配置文件

---
+++ miniprogram/app.js
@@ -0,0 +1,15 @@
+/**
+ * 冷库冷量计算小程序 - App入口
+ * 青岛冷锋节能工程有限公司
+ */
+App({
+  globalData: {
+    companyName: '青岛冷锋节能工程有限公司',
+    companyPhone: '13061468618',
+    companyAddress: '山东省青岛市'
+  },
+
+  onLaunch() {
+    console.log('冷库冷量计算小程序启动');
+  }
+});
